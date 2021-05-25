e85de91a0576932425939b02a6fe4333b4035c7a
stadzuki <st.adzuki@gmail.com>
2021-05-25 22:38:15 +0300

    filter bar

--- client/scripts/modules/filter.js
+++ client/scripts/modules/filter.js
@@ -1,24 +1,40 @@
 export default function () {
     const MAX_RANGE_WIDTH = 180; 
+    const MAX_DISTANCE_TO_SCROLL = 35;
+    const MIN_ITEM_PRICE = 5000;
+    const MAX_ITEM_PRICE = 25000;
 
     /* Вычеслять растояние между двумя скролами, а двигать бар только margin и тем временем высчитывать его ширину между двумя скролами */
 
     const scrollMin = document.querySelector('.skroll_min');
     const scrollMax = document.querySelector('.skroll_max');
-    const rangeBar = document.querySelector('.range__bar');    
+    const rangeBar = document.querySelector('.range__bar');  
+    const minPriceField = document.getElementById('min-price');
+    const maxPriceField = document.getElementById('max-price');
 
+    minPriceField.value = MIN_ITEM_PRICE;
+    maxPriceField.value = MAX_ITEM_PRICE;
+    
     scrollMin.style.transform = 'translateX(0px)';
     scrollMax.style.transform = 'translateX(0px)';
     rangeBar.style.width = `${MAX_RANGE_WIDTH}px`;
-
+    
     let scrollPos;
     let scrollMinPos = 0;
     let scrollMaxPos = 0;
-
+    
     let lastMovingPos = 0;
     let lastScrollMinPos = 0;
     let lastScrollMaxPos = 0;
+    
+    let minPriceValue = minPriceField.value;
+    let maxPriceValue = maxPriceField.value;
 
+    let counterLeft = 0;
+    let counterRight = 0;
+    
+    const calcStepPrice = Math.round((+minPriceField.value + +maxPriceField.value) / MAX_RANGE_WIDTH);
+    
     scrollMin.addEventListener('mousedown', onScrollMouseDown);
     scrollMax.addEventListener('mousedown', onScrollMouseDown);
 
@@ -43,57 +59,53 @@ export default function () {
         
         let posRightScroll = +getScrollPos(scrollMax);
         let posLeftScroll = +getScrollPos(scrollMin);
-        let sibling;
         
         const movingPos = e.clientX - scrollPos;
-        const border = MAX_RANGE_WIDTH - (posLeftScroll + -1 * (posRightScroll));
+        let limit = MAX_RANGE_WIDTH - (posLeftScroll + -1 * (posRightScroll));
 
         if(target.matches('.skroll_min')) {
-            sibling = target.nextElementSibling;
             lastMovingPos = lastScrollMinPos;
         } else if(target.matches('.skroll_max')) {
-            sibling = target.previousElementSibling;
             lastMovingPos = lastScrollMaxPos;
         }
 
         if(lastMovingPos > movingPos) { // to left
-            if(target.matches('.skroll_max') && border <= 20) {
-                return 1;
-            } else if(target.matches('.skroll_max')) {
-                let calculateBarValue = MAX_RANGE_WIDTH - (-1 * movingPos);
-                rangeBar.style.width = `${calculateBarValue}px`;
+            counterLeft++;
+
+            if(target.matches('.skroll_max') && limit <= MAX_DISTANCE_TO_SCROLL) {
+                return limit = MAX_DISTANCE_TO_SCROLL;
             }
             
             if(target.matches('.skroll_min') && posLeftScroll <= 0) {
                 return 1;
-            } else if(target.matches('.skroll_min')) {
-                let calculateBarValue = MAX_RANGE_WIDTH + (-1 * movingPos);
-                rangeBar.style.width = `${calculateBarValue}px`;
-                rangeBar.style.marginLeft = `${posLeftScroll + 2}px`;
+            } else {
+                rangeBar.style.marginLeft = `${posLeftScroll}px`;
             }
 
-            console.log('left')
+            console.log('дошло', counterLeft);
+            minPriceValue = +minPriceValue - 250;
+            minPriceField.value = minPriceValue;
         }
         
-        if(lastMovingPos < movingPos) { // to right
-            if(target.matches('.skroll_min') && border <= 20) {
-                return 1;
-            } else if(target.matches('.skroll_min')) {
-                let calculateBarValue = MAX_RANGE_WIDTH + (-1 * movingPos);
-                rangeBar.style.width = `${calculateBarValue}px`;
-                rangeBar.style.marginLeft = `${posLeftScroll + 3}px`;
-            }
+        if(lastMovingPos < movingPos) { // to right   
+            counterRight++;
             
             if(target.matches('.skroll_max') && posRightScroll >= 0) {
                 return 1;
-            } else if(target.matches('.skroll_max')) {
-                let calculateBarValue = MAX_RANGE_WIDTH - (-1 * movingPos);
-                rangeBar.style.width = `${calculateBarValue}px`;
-            }
+            } 
 
-            console.log('right')
+            if(target.matches('.skroll_min') && limit <= MAX_DISTANCE_TO_SCROLL) {
+                return 1;
+            } else {
+                rangeBar.style.marginLeft = `${posLeftScroll + 2}px`;
+            }            
+
+            console.log('дошло', counterRight);
+            minPriceValue = +minPriceValue + 250;
+            minPriceField.value = minPriceValue;
         }
 
+        rangeBar.style.width = `${limit}px`;
         target.style.transform = `translateX(${movingPos}px)`;
 
         target.matches('.skroll_min')
@@ -112,4 +124,5 @@ export default function () {
     function getScrollPos(elem) {
         return elem.style.transform.match(/\-?\d+/).join()
     }
+
 }
